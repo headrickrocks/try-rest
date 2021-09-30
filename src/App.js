1acf044cecb89a868d354447b4840c5ef54f8352
csn-headrickrocks <brentbartholomew04@gmail.com>
2021-09-30 13:48:34 -0400

    Deleting 2 other unnecessary table files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import Axios from "axios"
+import { useState } from 'react';
 
 const App = () =>{
+    const [joke, setJoke] = useState()
+    const getJoke = () => {
+      Axios.get("https://icanhazdadjoke.com/", {
+        headers: {
+          "Accept": "application/json"
+        }
+      }
+      ).then(
+        (response) => {
+          //console.log(response);
+          setJoke(response.data.joke);
+        }
+      );
+    };
   return (
-    <div className="container">
-      <p>mr-headrick</p>
+    <div className="container"><button onClick={getJoke}> Dad Joke </button>
+    <br/><p>{joke}</p>
     </div>
   );
 }
